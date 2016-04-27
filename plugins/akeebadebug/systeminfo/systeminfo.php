90b727a57bc83b689fad9d50a2127e3fd32ba556
Davide Tampellini <tampe125@gmail.com>
2016-04-27 12:33:18 +0200

    Created dummy class Added build file

---
+++ plugins/akeebadebug/systeminfo/systeminfo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package   DebugHelper
+ * @copyright Copyright (c)2016 Akeeba Ltd / Nicholas K. Dionysopoulos
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die;
+
+class plgAkeebadebugSysteminfo extends JPlugin
+{
+	
+}
---
+++ plugins/akeebadebug/systeminfo/version.php
@@ -0,0 +1,6 @@
+<?php
+// Protect from unauthorized access
+defined('_JEXEC') or die;
+
+define('DEBUGHELPER_VERSION', 'rev628D87D');
+define('DEBUGHELPER_DATE', '2016-04-27');
