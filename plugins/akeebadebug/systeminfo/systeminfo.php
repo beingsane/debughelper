a431d859fad6a432da1a4283df780adebad9c11f
Davide Tampellini <tampe125@gmail.com>
2016-04-29 10:46:31 +0200

    Initial version of the debug plugin

--- plugins/akeebadebug/systeminfo/systeminfo.php
+++ plugins/akeebadebug/systeminfo/systeminfo.php
@@ -9,5 +9,74 @@ defined('_JEXEC') or die;
 
 class plgAkeebadebugSysteminfo extends JPlugin
 {
-	
+	/**
+	 * Gather system information useful for support. If a component is passed we are going to add some relevant info for
+	 * that extension, too.
+	 *
+	 * @param $component
+	 *
+	 * @return array
+	 */
+	public function onGetSystemInformation($component = '')
+	{
+		$result = $this->genericInfo();
+
+		switch ($component)
+		{
+			case 'com_akeeba':
+				$result = array_merge($result, $this->akeebaBackupInfo());
+
+				break;
+			case 'com_admintools':
+				$result = array_merge($result, $this->adminToolsInfo());
+
+				break;
+			case 'com_ats':
+				$result = array_merge($result, $this->atsInfo());
+
+				break;
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Generic info about the system. For example database version, PHP version etc etc
+	 *
+	 * @return array
+	 */
+	private function genericInfo()
+	{
+		return array();
+	}
+
+	/**
+	 * System info useful for Akeeba Backup support.
+	 *
+	 * @return array
+	 */
+	private function akeebaBackupInfo()
+	{
+		return array();
+	}
+
+	/**
+	 * System info useful for Admin Tools support.
+	 *
+	 * @return array
+	 */
+	private function adminToolsInfo()
+	{
+		return array();
+	}
+
+	/**
+	 * System info useful for Akeeba Ticket System support.
+	 *
+	 * @return array
+	 */
+	private function atsInfo()
+	{
+		return array();
+	}
 }
